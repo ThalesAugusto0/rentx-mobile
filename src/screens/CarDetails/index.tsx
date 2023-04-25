7a3f225a7c408d2709017c9bdfb759be6b4f28e7
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-04-24 22:38:59 -0300

    Foi criado o a tela que mostra os detalhes do carro, com uma pequena descrição

---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Acessories,
+} from "./styles";
+import { BackButtton } from "../../components/BackButtton";
+import { ImageSlider } from "../../components/ImageSlider";
+import { Accessory } from "../../components/Accessory";
+import SpeedSvg from "../../assets/speed.svg";
+import AccelerationSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
+
+export function CarDetails() {
+  return (
+    <Container>
+      <Header>
+        <BackButtton onPress={() => {}} />
+      </Header>
+      <CarImages>
+        <ImageSlider
+          imagesUrls={[
+            "https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png",
+          ]}
+        />
+      </CarImages>
+      <Content>
+        <Details>
+          <Description>
+            <Brand>lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+          <Rent>
+            <Period>Ao Dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <Acessories>
+          <Accessory name="380Km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800 HP" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 lugares" icon={PeopleSvg} />
+        </Acessories>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praca Real Maestranza de Sevilla. É um belíssimo carro
+          para quem gosta de acelerar
+        </About>
+      </Content>
+    </Container>
+  );
+}
