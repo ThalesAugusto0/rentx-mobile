44afe73c4264290162f14e7553b1862dff761a3b
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-05-12 19:32:52 -0300

    Criado a splashScreen

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,6 +7,7 @@ import { SchedulingComplete } from "../screens/SchedulingComplete";
 import { SchedulingDatails } from "../screens/SchedulingDatails";
 import { MyCars } from "../screens/MyCars";
 import { Splash } from "../screens/Splash";
+import { SingIn } from "../screens/SingIn";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -14,6 +15,7 @@ export function StackRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }} initialRouteName="Splash">
       <Screen name="Splash" component={Splash} />
+      <Screen name="SingIn" component={SingIn} />
       <Screen name="Home" component={Home} />
       <Screen name="CarDetails" component={CarDetails} />
       <Screen name="Scheduling" component={Scheduling} />
