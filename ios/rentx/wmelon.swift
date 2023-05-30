eadb54aba454820545d3b96c3e13847dad0c9155
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-05-29 21:33:49 -0300

    Tentei arrumar um erro que não deixa eu buildar o ios

---
+++ ios/rentx/wmelon.swift
@@ -0,0 +1,9 @@
+
+//
+//  water.swift
+//  watermelonDB
+//
+//  Created by Watermelon-plugin on 29/05/2023.
+//
+
+import Foundation
---
+++ ios/wmelon.swift
@@ -0,0 +1,8 @@
+//
+//  wmelon.swift
+//  rentx
+//
+//  Created by Thales Augusto on 29/05/23.
+//
+
+import Foundation
